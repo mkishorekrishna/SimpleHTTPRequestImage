7ea64941c2fd5bb675f911351ef88b193ce487aa
kishorekris <kishorekris@vmware.com>
2020-06-09 16:31:55 +0530

    title

--- ImageRequest/ViewController.swift
+++ ImageRequest/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+
+//simple http
 enum KittenImageLocation: String {
     case http = "http://www.kittenswhiskers.com/wp-content/uploads/sites/23/2014/02/Kitten-playing-with-yarn.jpg"
     case https = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Kitten_in_Rizal_Park%2C_Manila.jpg/460px-Kitten_in_Rizal_Park%2C_Manila.jpg"
